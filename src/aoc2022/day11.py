983a4139b4404515d6176f1a81eb4143cab6b1c2
Kevin M Granger <git@kevinmgranger.me>
2022-12-11 22:37:58 -0500

    fix da issues

--- src/aoc2022/day11.py
+++ src/aoc2022/day11.py
@@ -1,11 +1,13 @@
-from common import input
-from dataclasses import dataclass, field
-from typing import Callable, NamedTuple, TypeAlias, Iterable, Self, TextIO
-from io import StringIO
-from functools import partial
 import operator
-from collections import deque
+import re
 import sys
+from collections import deque
+from dataclasses import dataclass, field
+from functools import partial
+from io import StringIO
+from typing import Callable, Iterable, NamedTuple, TextIO, TypeAlias
+
+from aoc2022.common import input
 
 
 class Targets(NamedTuple):
@@ -14,6 +16,7 @@ class Targets(NamedTuple):
 
     def to(self, result: bool) -> int:
         return self.true if result else self.false
+
 
 class InspectionResult(NamedTuple):
     item: int
@@ -26,6 +29,7 @@ Test: TypeAlias = Callable[[int], bool]
 
 @dataclass
 class Monke:
+    id: int
     items: deque[int]
     operation: Operation
     test: Test
@@ -34,18 +38,24 @@ class Monke:
     inspection_count: int = field(init=False, default=0)
 
     @classmethod
-    def parse(cls, spec: Iterable[str]) -> Self:
+    def parse(cls, id: int, spec: Iterable[str]):
         speciter = iter(spec)
 
         starting_items = deque(
-            int(item.strip()) for item in next(speciter).split(":")[1].strip().split(",")
-            )
-        op_spec = next(speciter).split("=").strip()
+            int(item.strip())
+            for item in next(speciter).split(":")[1].strip().split(",")
+        )
+        op_spec = next(speciter).split("=")[1].strip()
         test_num = int(next(speciter).split()[-1])
         true = int(next(speciter).split()[-1])
         false = int(next(speciter).split()[-1])
 
-        return cls(starting_items, compile_op(op_spec), compile_test(test_num), Targets(true, false))
+        return cls(id, 
+            starting_items,
+            compile_op(op_spec),
+            compile_test(test_num),
+            Targets(true, false),
+        )
 
     def inspect_one(self) -> InspectionResult | None:
         try:
@@ -92,27 +102,32 @@ def compile_op(op_spec: str) -> Operation:
 def compile_test(divisor: int) -> Test:
     return lambda item: (item % divisor) == 0
 
+
 @dataclass
 class Circus:
     monkeys: list[Monke]
     rounds: int = field(init=False, default=0)
 
     @classmethod
-    def parse(self, lines: Iterable[str]) -> Self:
+    def parse(cls, lines: Iterable[str]):
         line_iter = iter(lines)
 
-        next(line_iter) # skip first monke line
+        next(line_iter)  # skip first monke line
 
-        monke_spec = []
+        monke_spec: list[str] = []
+        monke_num = 0
 
         monkeys = []
-        
+
         for line in line_iter:
             if line.startswith("Monke"):
-                monkeys.append(Monke.parse(monke_spec))
+                monkeys.append(Monke.parse(monke_num, monke_spec))
                 monke_spec = []
+                monke_num += 1
             else:
                 monke_spec.append(line.strip())
+
+        monkeys.append(Monke.parse(monke_num, monke_spec))
 
         return cls(monkeys)
 
@@ -125,17 +140,16 @@ class Circus:
         for i in range(0, len(self.monkeys)):
             self.do_inspection_for(i)
 
-        self.round += 1
+        self.rounds += 1
 
     def status(self, out: TextIO = sys.stdout):
+        print(f"After round {self.rounds}, the monkeys are holding items with these worry levels:", file=out)
         for i, monkey in enumerate(self.monkeys):
             print("Monkey ", i, ": ", sep="", end="", file=out)
             print(*monkey.items, sep=", ", file=out)
-        
 
 
-
-class TEST:
+class Test:
     DATA = """Monkey 0:
   Starting items: 79, 98
   Operation: new = old * 19
@@ -164,8 +178,8 @@ Monkey 3:
     If true: throw to monkey 0
     If false: throw to monkey 1"""
 
+    AFTER_STATUS_RE = re.compile(r"^After round (\d+)")
     EXAMPLE_STATUSES = """After round 1, the monkeys are holding items with these worry levels:
-
 Monkey 0: 20, 23, 27, 26
 Monkey 1: 2080, 25, 167, 207, 401, 1046
 Monkey 2: 
@@ -226,7 +240,6 @@ Monkey 1: 481, 245, 22, 26, 1092, 30
 Monkey 2: 
 Monkey 3: 
 
-...
 
 After round 15, the monkeys are holding items with these worry levels:
 Monkey 0: 83, 44, 8, 184, 9, 20, 26, 102
@@ -234,10 +247,50 @@ Monkey 1: 110, 36
 Monkey 2: 
 Monkey 3: 
 
-...
 
 After round 20, the monkeys are holding items with these worry levels:
 Monkey 0: 10, 12, 14, 26, 34
 Monkey 1: 245, 93, 53, 199, 115
 Monkey 2: 
-Monkey 3:"""
+Monkey 3: """
+
+    def test_round_status(self):
+        circus = Circus.parse(self.DATA.splitlines())
+
+        def run_til_round(round: int):
+            while circus.rounds < round:
+                circus.do_round()
+
+        def check(lines: list[str]):
+            expected = "\n".join(lines) + "\n"
+
+            buf = StringIO()
+            circus.status(buf)
+
+            actual = buf.getvalue()
+
+            assert expected == actual
+
+        status_lines = self.EXAMPLE_STATUSES.splitlines()
+        collected_lines = [status_lines[0]]
+        round = 1
+
+        for line in status_lines[1:]:
+            if not line:
+                continue
+
+            if (match := self.AFTER_STATUS_RE.match(line)) is None:
+                collected_lines.append(line)
+                continue
+
+            # else matches
+            run_til_round(round)
+            check(collected_lines)
+
+            collected_lines = [line]
+            round = int(match[1])
+
+        run_til_round(round)
+        check(collected_lines)
+        
+
